7350b33d19e17babf112d5b971e6dc2f5a329eea
bendman <duncan.ben@gmail.com>
2018-09-17 18:21:54 -0700

    add modal and move served files into public folder

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
+/**
+ * Modal Setup
+ */
+window.addEventListener('click', (event) => {
+  const closeEl = event.target.closest('.modal-close');
+  if (closeEl) {
+    closeEl.closest('.modal').classList.add('-closed');
+  }
+});
+
 console.error(`
   “Without requirements or design, programming is the art of adding bugs to an empty text file.”
     - Louis Srygley
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,12 @@
+const path = require('path');
+
 module.exports = {
   mode: process.env.WEBPACK_SERVE ? 'development' : 'production',
   entry: {
     main: './src/index.js',
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, './public/build'),
   },
 };
